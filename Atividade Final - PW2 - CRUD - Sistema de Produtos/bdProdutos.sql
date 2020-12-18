c82e9ce209c28e76450b91fadcf677a45af23fd7
igoralmeidasouza <67607583+igoralmeidasouza@users.noreply.github.com>
2020-12-17 23:41:23 -0300

    Correção no código do BD

--- Atividade Final - PW2 - CRUD - Sistema de Produtos/bdProdutos.sql
+++ Atividade Final - PW2 - CRUD - Sistema de Produtos/bdProdutos.sql
@@ -3,7 +3,7 @@ create database dbProdutos CHARACTER SET utf8 COLLATE utf8_general_ci;
 use dbProdutos;
 
 create table tbprodutos(
-idProd int not null auto_increment,
+IdProd int not null auto_increment,
 nomeProd varchar(100),
 descricao varchar(100),
 marca varchar(100),
